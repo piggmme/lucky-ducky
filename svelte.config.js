02f8294e366345923b3c7fe7d90fc41d6b00cf11
cherry.k <cherry.k@kakaoenterprise.com>
2022-11-14 19:09:31 +0900

    conf: reset css 설정 #1

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,17 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
+    scss: {
+      prependData: `@import "./static/style.scss";`
+    }
+  }),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
