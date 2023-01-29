578e9729f3780adc925a8425385a16368135f1bf
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-01-29 16:55:26 +0900

    Merge branch 'develop' of https://github.com/kheeyaa/lucky-ducky into feature/main-page

---
+++ src/components/main/ItemCard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let title: string;
+  export let url: string;
+  export let thumbnail: string;
+</script>
+
+<div class="item-card">
+  <!-- <a href={url}>
+    <img class="image" src={`/imgs/thumbnails/${thumbnail}`} alt={title} />
+    <p>{title}</p>
+  </a> -->
+</div>
+
+<style>
+  .item-card {
+    width: 169px;
+    margin: 11px;
+    background-color: lightsalmon;
+  }
+  .image {
+    display: block;
+    width: 169px;
+    height: 117px;
+    margin: 0;
+    background-color: lightseagreen;
+  }
+</style>
