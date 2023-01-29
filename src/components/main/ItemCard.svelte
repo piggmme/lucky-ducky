085b17c9f401ec1bcfd5c0bd7ad893abd9dde08a
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-01-29 18:02:33 +0900

    feat: 메인 아이템카드 컴포넌트 개발

---
+++ src/components/main/ItemCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let title: string;
+  export let url: string;
+  export let thumbnail: string;
+</script>
+
+<div class="item-card">
+  <a href={url}>
+    <img class="image" src={`/imgs/thumbnails/${thumbnail}`} alt={title} />
+    <p>{title}</p>
+  </a>
+</div>
+
+<style lang="scss">
+  .item-card {
+    display: flex;
+  }
+  .image {
+    display: block;
+    margin: 0;
+  }
+</style>
