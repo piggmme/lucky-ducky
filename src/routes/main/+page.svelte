320753622d25231b016cf4b753448ed64d4e191e
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-01-29 18:18:48 +0900

    feat: 메인 페이지 개발

---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import ItemCard from '@/components/main/ItemCard.svelte';
+  const games = [
+    { title: '오리 경주', url: '/race', thumbnail: 'thumbnail_race.svg' },
+    { title: '오리 돌리기', url: '/spinDuck', thumbnail: 'thumbnail_spin.svg' },
+    {
+      title: '포춘쿠키',
+      url: '/fortuneCookie',
+      thumbnail: 'thumbnail_spin.svg'
+    },
+    { title: '오리 로또', url: '/lotto', thumbnail: 'thumbnail_lotto.svg' },
+    { title: '동전 던지기', url: '/coin', thumbnail: 'thumbnail_coin.svg' },
+    { title: '오리 사다리', url: '/ladder', thumbnail: 'thumbnail_ladder.svg' },
+    { title: '오리 뽑기', url: '/raffle', thumbnail: 'thumbnail_raffle.svg' },
+    { title: 'comming soon', url: '/raffle', thumbnail: 'thumbnail_raffle.svg' }
+  ];
+</script>
+
+<header>
+  <h1>게임 선택하기</h1>
+</header>
+<main>
+  <section>
+    <ul>
+      {#each games as game}
+        <li>
+          <ItemCard
+            title={game.title}
+            url={game.url}
+            thumbnail={game.thumbnail}
+          />
+        </li>
+      {/each}
+    </ul>
+  </section>
+</main>
+
+<!-- TODO: footer 레이아웃 분리 -->
+<footer>
+  <p>Contact Us</p>
+  <p>문의</p>
+  <p>제작</p>
+  <p>Instagram</p>
+  <p>Youtube</p>
+</footer>
+
+<style lang="scss">
+  header {
+    text-align: center;
+    font-size: 32px;
+  }
+  main {
+    max-width: 600px;
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+    list-style: none;
+  }
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    background-color: #cecece;
+  }
+</style>
