b4ef428a337257da9e9c4212c57be9f11f6e51a4
cherry.k <cherry.k@kakaoenterprise.com>
2023-01-29 18:02:56 +0900

    feat: 게임 이름 컴포넌트 생성

---
+++ src/components/common/GameName.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<div class="container">
+  <h1>
+    <slot />
+  </h1>
+</div>
+
+<style lang="scss">
+  .container {
+    width: 100%;
+    max-width: 600vw;
+    background-color: #cfcfcf;
+    text-align: center;
+    padding: 17px 0;
+  }
+  h1 {
+    color: #fff;
+    font-weight: 500;
+    font-size: 18px;
+  }
+</style>
