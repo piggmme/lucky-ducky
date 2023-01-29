b8446e6342eb0ac0042907bc8e0ac4ab0d2d4841
cherry.k <cherry.k@kakaoenterprise.com>
2023-01-29 14:18:01 +0900

    feat: 모바일 레이아웃 컴포넌트 생성

---
+++ src/Layout/MobileLayout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<main class="container">
+  <div class="view">
+    <slot />
+  </div>
+</main>
+
+<style lang="scss">
+  .container {
+    width: 100vw;
+    height: 100%;
+    background-color: #eaeaea;
+  }
+  .view {
+    max-width: 600px;
+    min-height: 100vh;
+    background-color: #fff;
+    margin: 0 auto;
+  }
+</style>
