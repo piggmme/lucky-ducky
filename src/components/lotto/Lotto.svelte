ace8291cc8d4a33091ed9881bc8200d41ce64272
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-01-30 18:30:47 +0900

    feat: 로또 번호 추출 기능 구현

---
+++ src/components/lotto/Lotto.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { suffleArray } from '@/utils/random';
+  import Button from '../common/button.svelte';
+
+  const numbers = new Array(46).fill(null).map((number, i) => i + 1);
+  let selectedNumbers = new Array(6).fill('?');
+  const selectNumbers = () => {
+    selectedNumbers = suffleArray(numbers)
+      .slice(0, 6)
+      .sort((a, b) => a - b);
+  };
+</script>
+
+<main>
+  <ul>
+    {#each selectedNumbers as number}
+      <li><span>{number}</span></li>
+    {/each}
+  </ul>
+
+  <Button handleClick={selectNumbers} buttonText={'공 뽑기!'} />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+</style>
