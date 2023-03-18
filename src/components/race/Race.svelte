ea127cf1cf5c3f5fb9b11b54f17c9e5e71fe3317
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-18 16:08:54 +0900

    :sparkles: 오리경주 디자인 적용

--- src/components/race/Race.svelte
+++ src/components/race/Race.svelte
@@ -4,12 +4,15 @@
   import Duck from './parts/Duck.svelte';
   import RankResult from './parts/RankResult.svelte';
   import type { duckType } from './type';
+  import RaceGudieLine from './parts/RaceGudieLine.svelte';
 
   let ducks: duckType[] = [{ name: '', id: 1, rank: 0, distanceX: 0 }];
   let isStart = false;
   let isReset = false;
   let rankings: duckType[] = [];
   let race: HTMLDivElement;
+
+  const totalDistance = 2200;
 
   const resetRace = () => {
     ducks = ducks.map((duck) => ({
@@ -80,7 +83,7 @@
     alt="오리 경주 배경"
     aria-hidden="true"
   />
-  <div class="button-wrapper">
+  <div class="header-button-wrapper">
     <button class="duck-add" on:click={addDuck} type="button"
       ><img width="20" src="/imgs/race/add.svg" alt="추가" aria-hidden="true" />
       오리 추가</button
@@ -93,17 +96,21 @@
 </div>
 
 <div bind:this={race} class="race">
+  <RaceGudieLine {totalDistance} isBottomBorder={false} />
   {#each ducks as duck (duck.id)}
     <div class="race-field">
-      <div class="ducky-number">{duck.id}번 덕희</div>
-      <Duck
-        setDuckRanking={setDuckRanking(duck)}
-        bind:isStart
-        bind:isReset
-        bind:distanceX={duck.distanceX}
-      />
-      <RankResult rank={duck.rank} lastLank={ducks.length} />
-      <div class="race-goal" />
+      <div class="race-duck">
+        <RaceGudieLine {totalDistance} />
+        <div class="race-number">{duck.id}번 덕희</div>
+        <Duck
+          setDuckRanking={setDuckRanking(duck)}
+          bind:isStart
+          bind:isReset
+          bind:distanceX={duck.distanceX}
+        />
+        <div class="race-space" style={`width: ${totalDistance}px`} />
+        <RankResult {duck} rank={duck.rank} lastLank={ducks.length} />
+      </div>
     </div>
   {/each}
 </div>
@@ -111,58 +118,58 @@
 <style lang="scss">
   .header {
     position: relative;
-  }
-  .button-wrapper {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
 
-    .start-reset-wrapper {
+    &-button-wrapper {
       position: absolute;
-      display: flex;
-      flex-direction: column;
-      top: 30%;
-      right: 50%;
-      transform: translateX(50%);
-    }
-    .start {
-      background-color: #f6ce55;
-      border: 1.2px solid #000;
-      padding: 15px 77px;
-      border-radius: 7px;
-      font-size: 18px;
-      letter-spacing: 0.38em;
-    }
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
 
-    .reset {
-      color: #fff;
-      opacity: 0.6;
-      padding-top: 7px;
-    }
+      .start-reset-wrapper {
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        top: 30%;
+        right: 50%;
+        transform: translateX(50%);
+      }
+      .start {
+        background-color: #f6ce55;
+        border: 1.2px solid #000;
+        padding: 15px 77px;
+        border-radius: 7px;
+        font-size: 18px;
+        letter-spacing: 0.38em;
+      }
 
-    .duck-add {
-      position: absolute;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      gap: 7px;
-      top: 5%;
-      right: 5%;
-      background-color: #fff;
-      border: 1.2px solid #000;
-      border-radius: 5px;
-      padding: 9px 14px;
+      .reset {
+        color: #fff;
+        opacity: 0.6;
+        padding-top: 7px;
+      }
+
+      .duck-add {
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 7px;
+        top: 5%;
+        right: 5%;
+        background-color: #fff;
+        border: 1.2px solid #000;
+        border-radius: 5px;
+        padding: 9px 14px;
+      }
     }
   }
+
   .race {
-    padding-top: 30px;
+    padding-top: 40px;
     overflow-x: scroll;
-    overflow-y: visible;
     width: 100%;
-    height: 100%;
-    transform: translateY(-60px);
+    position: relative;
 
     &-field {
       display: flex;
@@ -170,26 +177,32 @@
       position: relative;
       justify-content: start;
       align-items: center;
-      width: 2200px;
-      height: 63px;
+      height: 70px;
     }
 
-    &-goal {
+    &-duck {
       position: absolute;
-      top: 0;
-      right: 150px;
-      width: 1px;
-      height: 100%;
-      background-color: black;
+      bottom: 0;
+      display: flex;
+      justify-content: space-around;
+      align-items: end;
+      padding: 10px;
+      height: 70px;
+      border-bottom: 2px solid #ccc;
     }
-  }
 
-  .ducky-number {
-    background-color: #16701b;
-    padding: 7px 14px;
-    border-radius: 5px;
-    border: 1px solid #000;
-    color: #fff;
-    margin: 60px 10px 0px;
+    &-number {
+      background-color: #16701b;
+      padding: 7px 14px;
+      border-radius: 5px;
+      border: 1px solid #000;
+      color: #fff;
+      flex-shrink: 0;
+      margin: 10px;
+    }
+
+    &-space {
+      height: 70px;
+    }
   }
 </style>
--- src/components/race/parts/RankResult.svelte
+++ src/components/race/parts/RankResult.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+  import type { duckType } from '../type';
+
+  export let duck: duckType;
   export let rank = 0;
   export let lastLank: number;
 </script>
 
-<div class="rank">
+<div class={`rank ${rank === 1 ? 'win' : rank === lastLank ? 'lose' : ''}`}>
+  {#if rank !== 0}
+    <div>{duck.name || `${duck.id}번 덕희`}</div>
+  {/if}
+
   {#if rank === 0}
     <div />
   {:else if rank === 1}
@@ -15,11 +22,23 @@
   {/if}
 </div>
 
-<style>
+<style lang="scss">
   .rank {
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    right: 180px;
+    background-color: rgba(171, 231, 255, 0.512);
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    transform: translateX(-30%);
+
+    &.win {
+      background-color: rgba(171, 255, 177, 0.512);
+    }
+
+    &.lose {
+      background-color: rgba(255, 171, 171, 0.512);
+    }
   }
 </style>
